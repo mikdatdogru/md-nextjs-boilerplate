0dd86d6461d751ffe0810aa69a38a29c0ca0084d
Mikdat Dogru <mikdatdogru@gmail.com>
2019-05-29 05:13:54 +0300

    all dependencies update

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,14 +1,18 @@
 import React from 'react';
 
-export default ({ todo, remove }) => (
-	<li style={{ listStyle: 'none' }}>
-		<button
-			className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored mdl-js-ripple-effect"
-			onClick={_ => remove(todo)}
-			style={{ fontSize: 12 }}
-		>
-			x
-		</button>{' '}
-		{todo.text}
-	</li>
-);
+const TodoItem = ({ todo, remove }) => {
+	return (
+		<li style={{ listStyle: 'none' }}>
+			<button
+				className="mdl-button mdl-js-button mdl-button--fab mdl-button--mini-fab mdl-button--colored mdl-js-ripple-effect"
+				onClick={() => remove(todo)}
+				style={{ fontSize: 12 }}
+			>
+				x
+			</button>{' '}
+			{todo.text}
+		</li>
+	);
+};
+
+export default TodoItem;
--- utils/store.js
+++ utils/store.js
@@ -1,14 +1,17 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 
 import rootReducer from '../reducers';
-
+/* eslint-disable no-underscore-dangle */
 const enhancers = compose(
 	typeof window !== 'undefined' && process.env.NODE_ENV !== 'production'
-		? window.devToolsExtension && window.devToolsExtension()
+		? window.__REDUX_DEVTOOLS_EXTENSION__ &&
+				window.__REDUX_DEVTOOLS_EXTENSION__({ serialize: true, trace: true })
 		: f => f
 );
-
+/* eslint-enable */
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 export default initialState =>
 	createStoreWithMiddleware(rootReducer, initialState, enhancers);
+
+
